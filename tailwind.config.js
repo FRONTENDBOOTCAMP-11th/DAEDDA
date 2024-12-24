51dab477301713ad513ad42cc4882885d05bf260
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-24 19:54:51 +0900

    💄 style: 전역 폰트 설정, 테일윈드 css 적용, 테스트 파일 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pre: ["Pretendard"],
+      },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        white: "#FFFFFF",
+        black: "#222222",
+        red: "#FF0000",
+        primary: "#8C6FEE",
+        secondary: "#BCA6F4",
+        "purple-100": "#EBD5FE",
+        "purple-900": "#6123FF",
+        "beige-500": "#CDC3C3",
+        "gray-200": "#D9D9D9",
+        "gray-500": "#555555",
       },
     },
   },
