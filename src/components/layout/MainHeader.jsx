ae1672d864cb20a2b21a81d1cc623a34b488e8bb
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-26 16:15:25 +0900

    💄 style: 쉐도우 속성 변경, 헤더 종 모양 아이콘 변경

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -3,7 +3,7 @@ export default function MainHeader() {
     <header className="w-full h-[60px] flex items-center justify-between mb-5">
       <img src="/src/assets/logos/header-logo.png" className="w-[190px] ml-1" />
       <div className="flex items-center gap-4">
-        <img src="/icons/purpleAlarm.svg" />
+        <img src="/icons/alarm.svg" />
         <img src="/icons/hamburger.svg" className="w-6" />
       </div>
     </header>
