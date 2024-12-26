cb6991939b334d2d3f8e429f1070120ef097528e
Donghwan Oh <imodh97@gmail.com>
2024-12-26 22:57:19 +0900

    ✨ feat: text area 인풋 옵션 추가가

--- src/components/layout/InputField.jsx
+++ src/components/layout/InputField.jsx
@@ -5,14 +5,18 @@ InputField.propTypes = {
   labelName: PropTypes.string,
   type: PropTypes.string,
   placeholder: PropTypes.string,
-  className: PropTypes.string,
+  errorMsg: PropTypes.string,
+  isLast: PropTypes.bool,
+  isTextArea: PropTypes.bool,
 };
 
 export default function InputField({
   labelName = "",
   type = "text",
   placeholder = "",
-  className = "",
+  errorMsg = "",
+  isLast = false,
+  isTextArea = false,
 }) {
   // type에 따라 전달할 props 객체를 다르게 저장하는 변수
   const dateTypeProps =
@@ -23,19 +27,32 @@ export default function InputField({
           onFocus: e => (e.target.type = "date"),
           onBlur: e => {
             if (!e.target.value) e.target.type = "text";
-          }}
+          },
+        }
+      : { type, placeholder };
+
+  return (
+    <div>
+      {labelName && <p className="text-base font-semibold mb-2">{labelName}</p>}
+
+      {isTextArea ? (
+        <textarea
+          {...dateTypeProps}
+          className={`w-full p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"}`}
+          rows={10}
         />
-      </>
-    );
-  else {
-    return (
-      <>
+      ) : (
         <input
-          type={type}
-          placeholder={placeholder}
-          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className}`}
+          {...dateTypeProps}
+          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"}`}
         />
-      </>
-    );
-  }
+      )}
+
+      {errorMsg && (
+        <p
+          className={`text-red text-xs ${isLast ? "" : "mb-2"}`}
+        >{`*${errorMsg}`}</p>
+      )}
+    </div>
+  );
 }
