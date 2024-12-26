44d2de91d8854f038ff0279bc81c38f2c7ab2d6c
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-26 17:57:52 +0900

    💄 style: input 높이 변경

--- src/pages/main/MainWrite.jsx
+++ src/pages/main/MainWrite.jsx
@@ -11,20 +11,20 @@ export default function MainWrite() {
               type="text"
               id="subject"
               placeholder="제목"
-              className="mt-1 block w-full h-[32px] pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
           </div>
 
           <fieldset className="mt-7">
             <legend className="text-[16px] font-bold mb-2">위치</legend>
-            <div className="max-w-screen-sm h-24 bg-slate-500 mb-7 rounded-lg flex items-center justify-center">
+            <div className="w-full max-w-screen-sm h-24 bg-slate-500 mb-7 rounded-lg flex items-center justify-center">
               지도
             </div>
             <input
               type="text"
               id="address"
               placeholder="상세 주소"
-              className="mt-1 block w-full h-[32px] pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
           </fieldset>
 
@@ -35,7 +35,7 @@ export default function MainWrite() {
               id="brand"
               placeholder="가게 이름"
               aria-label="가게 이름"
-              className="mt-1 block w-full h-[32px] pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <input
@@ -43,7 +43,7 @@ export default function MainWrite() {
               id="pay"
               placeholder="급여"
               aria-label="급여"
-              className="mt-7 block w-full h-[32px] pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-7 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <input
@@ -51,7 +51,7 @@ export default function MainWrite() {
               id="timeInput"
               aria-label="근무 시간"
               placeholder="근무 시간은 00:00 - 00:00 형식으로 작성해주세요."
-              className="mt-7 block w-full h-[32px] pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-7 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <div>
@@ -59,7 +59,7 @@ export default function MainWrite() {
                 type="date"
                 id="dateInput"
                 aria-label="근무 날짜"
-                className="mt-7 block w-full h-[32px] pl-3 pr-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary text-gray-400 focus:border-2"
+                className="mt-7 block w-full h-12 pl-3 pr-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary text-gray-400 focus:border-2"
                 defaultValue={new Date().toISOString().slice(0, 10)}
               />
             </div>
