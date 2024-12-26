55c467bc54827ab59141e4d870ff42fe2a322621
Donghwan Oh <imodh97@gmail.com>
2024-12-26 23:08:41 +0900

    ✨ feat: input의 기본 props도 컴포넌트에 전달할 수 있게 수정

--- src/components/layout/InputField.jsx
+++ src/components/layout/InputField.jsx
@@ -17,6 +17,7 @@ export default function InputField({
   errorMsg = "",
   isLast = false,
   isTextArea = false,
+  ...props
 }) {
   // type에 따라 전달할 props 객체를 다르게 저장하는 변수
   const dateTypeProps =
@@ -40,11 +41,13 @@ export default function InputField({
           {...dateTypeProps}
           className={`w-full p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"}`}
           rows={10}
+          {...props}
         />
       ) : (
         <input
           {...dateTypeProps}
           className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"}`}
+          {...props}
         />
       )}
 
--- src/pages/main/MainList.jsx
+++ src/pages/main/MainList.jsx
@@ -9,7 +9,7 @@ export default function MainList() {
         type="date"
         errorMsg="생년월일 입력은 필수입니다."
       />
-      <InputField placeholder="기본 텍스트 인풋" />
+      <InputField placeholder="기본 텍스트 인풋" maxLength="5" />
       <InputField labelName="이메일" placeholder="이메일을 입력해 주세요." />
       <InputField
         labelName="내용"
