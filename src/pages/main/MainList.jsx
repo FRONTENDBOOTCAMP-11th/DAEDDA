9943212089a2e5c7f1ed5c266b8d26d85ebbc11f
Donghwan Oh <imodh97@gmail.com>
2024-12-26 17:13:05 +0900

    ✨ feat: 메인 리스트 페이지 필터 추가가

--- src/pages/main/MainList.jsx
+++ src/pages/main/MainList.jsx
@@ -9,16 +9,29 @@ export default function MainList() {
         </div>
       </div>
 
-      <div className="py-[14px]">
-        <label htmlFor="time" className="mr-[16px] font-[700] ">
-          시간
-        </label>
-        <select className="ring-2 ring-gray-400 focus:ring-primary py-[9px] px-[25px] rounded-xl">
-          <option>0 ~ 4시간</option>
-          <option>4 ~ 10시간</option>
-          <option>10시간 이상</option>
-        </select>
+      <div className="py-[14px] flex gap-4 mb-5">
+        <div>
+          <label htmlFor="time" className="mr-[16px] font-[700] ">
+            시간
+          </label>
+          <select className="ring-2 ring-gray-400 focus:ring-primary py-[9px] px-3 rounded-xl">
+            <option>0 ~ 4시간</option>
+            <option>4 ~ 10시간</option>
+            <option>10시간 이상</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="time" className="mr-[16px] font-[700] ">
+            급여
+          </label>
+          <select className="ring-2 ring-gray-400 focus:ring-primary py-[9px] px-3 rounded-xl">
+            <option>시급 10,000원 이하</option>
+            <option>시급 10,000원 이상</option>
+          </select>
+        </div>
       </div>
+
+      <div></div>
     </div>
   );
 }
