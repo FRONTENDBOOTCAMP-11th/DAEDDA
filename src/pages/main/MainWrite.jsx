e13ffa291a83994cb70f6481634a4b960d24aeb5
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-26 18:02:50 +0900

    💄 style: input padding 변경

--- src/pages/main/MainWrite.jsx
+++ src/pages/main/MainWrite.jsx
@@ -11,7 +11,7 @@ export default function MainWrite() {
               type="text"
               id="subject"
               placeholder="제목"
-              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
           </div>
 
@@ -24,7 +24,7 @@ export default function MainWrite() {
               type="text"
               id="address"
               placeholder="상세 주소"
-              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
           </fieldset>
 
@@ -35,7 +35,7 @@ export default function MainWrite() {
               id="brand"
               placeholder="가게 이름"
               aria-label="가게 이름"
-              className="mt-1 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <input
@@ -43,7 +43,7 @@ export default function MainWrite() {
               id="pay"
               placeholder="급여"
               aria-label="급여"
-              className="mt-7 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <input
@@ -51,7 +51,7 @@ export default function MainWrite() {
               id="timeInput"
               aria-label="근무 시간"
               placeholder="근무 시간은 00:00 - 00:00 형식으로 작성해주세요."
-              className="mt-7 block w-full h-12 pl-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
             />
 
             <div>
@@ -59,7 +59,7 @@ export default function MainWrite() {
                 type="date"
                 id="dateInput"
                 aria-label="근무 날짜"
-                className="mt-7 block w-full h-12 pl-3 pr-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary text-gray-400 focus:border-2"
+                className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary text-gray-400 focus:border-2"
                 defaultValue={new Date().toISOString().slice(0, 10)}
               />
             </div>
