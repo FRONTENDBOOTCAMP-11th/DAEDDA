858d39a84abdf896f1d79c1fe57064669c66681e
JeonSuna <jsa020910@gmail.com>
2024-12-26 22:57:19 +0900

    💄 style: btn 컴포넌트

--- src/components/layout/Button.jsx
+++ src/components/layout/Button.jsx
@@ -1,3 +1,72 @@
-export default function Button() {
-  return <div>Button</div>;
+import PropTypes from "prop-types";
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  color: PropTypes.string,
+  height: PropTypes.string,
+  width: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default function Button({ children, onClick, color, width, height }) {
+  let combinedClassName = "";
+
+  // 색
+  switch (color) {
+    case "purple": {
+      combinedClassName +=
+        " bg-primary text-white font-bold w-full mb-4 rounded";
+      break;
+    }
+    case "white": {
+      combinedClassName +=
+        " bg-white text-primary font-bold w-full mb-4 border border-primary rounded";
+      break;
+    }
+    case "red": {
+      combinedClassName += " bg-red text-white font-bold mb-4 rounded";
+      break;
+    }
+    default:
+      break;
+  }
+
+  // 높이
+  switch (height) {
+    case "sm": {
+      combinedClassName += " h-8";
+      break;
+    }
+    case "md": {
+      combinedClassName += " h-9";
+      break;
+    }
+    case "lg": {
+      combinedClassName += " h-12";
+      break;
+    }
+    default:
+      break;
+  }
+
+  // 넓이이
+  switch (width) {
+    case "xl": {
+      combinedClassName += " w-16 text-[14px]";
+      break;
+    }
+    case "2xl": {
+      combinedClassName += " w-[92px] text-[14px]";
+      break;
+    }
+    default:
+      break;
+  }
+
+  return (
+    <button className={`${combinedClassName}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
