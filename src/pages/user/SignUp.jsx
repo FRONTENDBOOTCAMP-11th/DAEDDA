c34b3d2d22f91e81c9d1cd16c1ee42dc8ff2cad0
yeon1ee <carpediem.y13@gmail.com>
2024-12-26 20:11:05 +0900

    🚧 cont: 회원 가입 페이지 마크업 작업

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -1,14 +1,14 @@
 export default function SignUp() {
   return (
     // 화면 전체 가운데 정렬, 640px
-    <div className="max-w-screen-sm h-screen m-auto bg-slate-400">
+    <div className="max-w-screen-sm h-screen m-auto">
       {/* 내부의 모든 요소들 세로로 */}
       <div className="flex flex-col items-center justify-center">
         <img
           src="/src/assets/images/smiling_daeddamon.png"
           className="w-[150px] h-[150px]"
         />
-        <button className="py-2 px-[14px] mt-2 bg-primary rounded-lg text-white text-sm mb-3">
+        <button className="py-2 px-[14px] mt-2 bg-primary rounded-lg text-white text-sm mb-3 font-bold test-sm">
           이미지 선택
         </button>
 
@@ -70,6 +70,15 @@ export default function SignUp() {
         <p className="self-start mt-2 mb-2 text-xs text-red">
           휴대폰 번호를 입력해주세요.
         </p>
+
+        <div className="flex gap-6 w-full">
+          <button className="border border-primary rounded-lg h-[48px] flex-1 items-center justify-center mb-2 text-sm text-primary font-bold text-lg">
+            취소
+          </button>
+          <button className="bg-primary border-none rounded-lg h-[48px] flex-1 flex items-center justify-center mb-2 text-sm text-white font-bold text-lg">
+            계속
+          </button>
+        </div>
       </div>
     </div>
   );
