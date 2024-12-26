f232a6df3035a897edf8c3e0c63bd10327aba0df
yeon1ee <carpediem.y13@gmail.com>
2024-12-26 23:35:50 +0900

    ✨ feat: 약관 페이지 마크업, logo 이미지 수정

--- src/pages/user/Terms.jsx
+++ src/pages/user/Terms.jsx
@@ -1,3 +1,39 @@
 export default function Terms() {
-  return <div>약관 페이지</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-center w-full bg-slate-300">
+        <img src="/src/pages/user/logo.png" className="max-w-full h-[100px]" />
+      </div>
+      <h1>환영합니다!</h1>
+      <p>약관에 동의 하셔야 회원 가입이 가능합니다.</p>
+      <p>약관 동의</p>
+      <div className="">
+        <input
+          type="checkbox"
+          id="check-all"
+          className="accent-primary border-primary"
+        ></input>
+        <label htmlFor="check-all">전체 동의</label>
+      </div>
+      <div className="">
+        <input
+          type="checkbox"
+          id="check-all"
+          className="accent-primary border-primary"
+        ></input>
+        <label htmlFor="check-all">(필수) 서비스 이용 약관 동의</label>
+      </div>
+      <div className="">
+        <input
+          type="checkbox"
+          id="check-all"
+          className="accent-primary border-primary"
+        ></input>
+        <label htmlFor="check-all">(필수) 서비스 이용 약관 동의</label>
+      </div>
+      <p>* 미동의 텍스트</p>
+      <button></button>
+      <button></button>
+    </div>
+  );
 }
