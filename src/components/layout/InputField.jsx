eabe342bf19d48599f1cdc9535f0571c1c199426
Donghwan Oh <imodh97@gmail.com>
2024-12-26 21:13:29 +0900

    💄 style: proptypes 추가, border => ring으로 변경경

--- src/components/layout/InputField.jsx
+++ src/components/layout/InputField.jsx
@@ -1,4 +1,12 @@
+import PropTypes from "prop-types";
+
 // 회원가입, 로그인 기준 input
+InputField.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+};
+
 export default function InputField({
   type = "text",
   placeholder = "",
@@ -9,7 +17,8 @@ export default function InputField({
       <input
         type={type}
         placeholder={placeholder}
-        className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className}`}
+        // className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className}`}
+        className={`w-full h-12 rounded-lg ring-2 ring-[#999] focus:outline-none focus:ring-primary p-3${className}`}
       />
     </>
   );
