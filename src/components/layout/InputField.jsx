521e01cd010f5e6d21b61d589aa838a34cf9cd23
Donghwan Oh <imodh97@gmail.com>
2024-12-26 21:38:33 +0900

    ✨ feat: date일 때 js 이용해 placeholder 추가, ring 대신 border 다시 사용용

--- src/components/layout/InputField.jsx
+++ src/components/layout/InputField.jsx
@@ -12,14 +12,30 @@ export default function InputField({
   placeholder = "",
   className = "",
 }) {
-  return (
-    <>
-      <input
-        type={type}
-        placeholder={placeholder}
-        // className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className}`}
-        className={`w-full h-12 rounded-lg ring-2 ring-[#999] focus:outline-none focus:ring-primary p-3${className}`}
-      />
-    </>
-  );
+  // date type input에 placeholder 색을 입히기 위함
+  if (type === "date")
+    return (
+      <>
+        <input
+          type="text"
+          placeholder="연도-월-일"
+          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className} placeholder-gray-400`}
+          onFocus={e => (e.target.type = "date")}
+          onBlur={e => {
+            if (!e.target.value) e.target.type = "text";
+          }}
+        />
+      </>
+    );
+  else {
+    return (
+      <>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${className}`}
+        />
+      </>
+    );
+  }
 }
