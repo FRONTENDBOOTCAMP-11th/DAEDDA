54ab7e50c53eb995fb60bb8255489fed21669193
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-26 16:56:09 +0900

    🔧 conf: 설정 색상 추가 및 폰트 가중치 추가 #20

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pre: ["Pretendard-Regular"],
+        pre: ["Pretendard"],
       },
       boxShadow: {
         "custom-bright": "-1px -1px 16px rgba(238, 238, 238, 1)",
@@ -25,6 +25,7 @@ export default {
         "purple-900": "#6123FF",
         "beige-500": "#CDC3C3",
         "gray-200": "#D9D9D9",
+        "gray-300": "999999",
         "gray-500": "#555555",
         "yellow-100": "#FEE500",
       },
