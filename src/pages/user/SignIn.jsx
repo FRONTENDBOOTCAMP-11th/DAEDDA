760a517989c95d7c534322dade3e69fae7ca406c
yeon1ee <carpediem.y13@gmail.com>
2024-12-27 11:40:34 +0900

    🚧 cont: 로그인 마크업 작업중

--- src/pages/user/SignIn.jsx
+++ src/pages/user/SignIn.jsx
@@ -1,3 +1,6 @@
+import Button from "@components/layout/Button";
+import InputField from "@components/layout/InputField";
+
 export default function SignIn() {
   return (
     <div className="flex flex-col items-center justify-center p-4">
@@ -7,33 +10,30 @@ export default function SignIn() {
         className="my-7 w-[180px] h-[180px]"
       />
 
-      <input
-        type="email"
-        placeholder="이메일"
-        className="w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary"
-        maxLength="30"
-      />
-      <p className="self-start mt-2 mb-2 text-xs text-red">
-        이메일을 입력해주세요.
-      </p>
+      <div className="w-full">
+        <InputField
+          type="email"
+          placeholder="이메일"
+          errorMsg="이메일을 입력해주세요."
+          maxLength="30"
+        ></InputField>
+      </div>
 
       <div className="relative w-full">
-        <input
+        <InputField
           type="password"
           placeholder="비밀번호"
-          className="w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary"
+          errorMsg="비밀번호를 입력해주세요."
           maxLength="20"
-        />
+        ></InputField>
         <img
           src="/public/icons/eyeHalf.svg"
           className="absolute right-3 top-3"
         />
       </div>
-      <p className="self-start mt-2 mb-2 text-xs text-red">
-        비밀번호를 입력해주세요.
-      </p>
 
-      <button className="bg-primary border-none rounded-lg h-[60px] w-full flex items-center justify-center mb-2 text-sm text-white">
+      <Button color="purple" width="2xl"></Button>
+      {/* <button className="bg-primary border-none rounded-lg h-[60px] w-full flex items-center justify-center mb-2 text-sm text-white">
         로그인
       </button>
       <button className="bg-yellow-100 border-none rounded-lg h-[60px] w-full flex items-center justify-center relative">
@@ -42,7 +42,7 @@ export default function SignIn() {
           className="absolute left-4 w-6 h-6"
         />
         <span className="text-black text-sm">카카오 로그인</span>
-      </button>
+      </button> */}
 
       <div className="flex items-center justify-center gap-2 mt-2">
         <p className="font-bold text-sm">대타를 찾고 있다면?</p>
