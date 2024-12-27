811151bb1faa33a2957aeceb78d490a9c9d279c0
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-27 20:50:38 +0900

    💄 style: input, button 요소들 공통 컴포넌트의 input, button으로 변경

--- src/pages/main/MainWrite.jsx
+++ src/pages/main/MainWrite.jsx
@@ -1,17 +1,18 @@
+import Button from "@components/layout/Button";
+import InputField from "@components/layout/InputField";
+
 export default function MainWrite() {
   return (
     <div className="min-h-screen">
       <div className="w-full">
-        <form className="max-w-screen-sm purple-100">
+        <form className="max-w-screen-sm">
           <div className="mt-5">
-            <label htmlFor="subject" className="text-[16px] font-bold mb-2">
-              제목
-            </label>
-            <input
+            <InputField
+              labelName="제목"
+              id="title"
               type="text"
-              id="subject"
               placeholder="제목"
-              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              errorMsg="제목 입력은 필수입니다."
             />
           </div>
 
@@ -20,68 +21,55 @@ export default function MainWrite() {
             <div className="w-full max-w-screen-sm h-24 bg-slate-500 mb-7 rounded-lg flex items-center justify-center">
               지도
             </div>
-            <input
+            <InputField
               type="text"
               id="address"
               placeholder="상세 주소"
-              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              errorMsg="주소 입력은 필수입니다."
+              isLast={true}
             />
           </fieldset>
 
           <fieldset className="mt-7">
-            <legend className="text-[16px] font-bold mb-2">근무 조건</legend>
-            <input
+            <InputField
+              labelName="근무 조건"
               type="text"
-              id="brand"
               placeholder="가게 이름"
-              aria-label="가게 이름"
-              className="mt-1 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              errorMsg="가게 이름 입력은 필수입니다."
             />
 
-            <input
-              type="number"
-              id="pay"
-              placeholder="급여"
-              aria-label="급여"
-              className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
-            />
-
-            <input
+            <InputField
               type="text"
-              id="timeInput"
-              aria-label="근무 시간"
-              placeholder="근무 시간은 00:00 - 00:00 형식으로 작성해주세요."
-              className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
+              placeholder="급여"
+              errorMsg="급여 입력은 필수입니다."
             />
 
-            <div>
-              <input
-                type="date"
-                id="dateInput"
-                aria-label="근무 날짜"
-                className="mt-7 block w-full h-12 p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary text-gray-400 focus:border-2"
-                defaultValue={new Date().toISOString().slice(0, 10)}
-              />
-            </div>
+            <InputField
+              type="text"
+              errorMsg="근무 시간 입력은 필수입니다."
+              placeholder="근무 시간은 00:00 - 00:00으로 입력해주세요."
+            />
+            <InputField
+              type="date"
+              errorMsg="생년월일 입력은 필수입니다."
+              isLast={true}
+            />
           </fieldset>
 
           <fieldset className="mt-7">
-            <legend className="text-[16px] font-bold mb-2">근무 내용</legend>
-            <textarea
-              id="workCondition"
-              rows="10"
-              placeholder="업무에 대한 자세한 설명"
-              className="mt-1 block w-full h-[120px] p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:border-2"
-            ></textarea>
+            <InputField
+              type="text"
+              labelName="근무 내용"
+              id="workTxt"
+              errorMsg="근무 내용 입력은 필수입니다."
+              isLast={true}
+              isTextArea={true}
+            />
           </fieldset>
-
-          <div>
-            <button
-              type="submit"
-              className="w-full mt-11 rounded-lg h-12 text-white font-bold bg-[#8C6FEE]"
-            >
+          <div className="mt-11">
+            <Button color="purple" height="lg" type="submit">
               등록
-            </button>
+            </Button>
           </div>
         </form>
       </div>
