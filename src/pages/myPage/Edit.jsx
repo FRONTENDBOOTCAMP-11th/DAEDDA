7f5c6eafad314f7f5045fa9ae651a99384cb1b0e
JeonSuna <jsa020910@gmail.com>
2024-12-28 20:49:18 +0900

    ♻️ refactor: input 컴포넌트 적용용

--- src/pages/myPage/Edit.jsx
+++ src/pages/myPage/Edit.jsx
@@ -1,3 +1,5 @@
+import InputField from "@components/layout/InputField";
+
 export default function Edit() {
   return (
     <>
@@ -11,61 +13,34 @@ export default function Edit() {
           save change
         </p>
       </div>
-      <div>
-        <p className="text-base font-semibold mb-2">닉네임</p>
-        <input
-          className="w-full border-2 border-gray-300 p-3 h-12
-          rounded-lg focus:border-secondary focus:border-2 focus:outline-none mb-2"
-          type="text"
-          placeholder="이메일을 입력해 주세요"
-        />
-        <p className="text-red text-xs mb-2">*미입력 알림 텍스트 부분</p>
-      </div>
-      <div>
-        <p className="text-base font-semibold mb-2">이메일</p>
-        <input
-          className="w-full border-2 border-gray-300 p-3 h-12
-          rounded-lg focus:border-primary focus:border-2 focus:outline-none mb-2"
-          type="text"
-          placeholder="아이디를 입력해 주세요"
-        />
-        <p className="text-red text-xs mb-2">*미입력 알림 텍스트 부분</p>
-      </div>
-      <div>
-        <p className="text-base font-semibold mb-2">아이디</p>
-        <input
-          className="w-full border-2 border-gray-300 p-3 h-12
-          rounded-lg focus:border-primary focus:border-2 focus:outline-none mb-2"
-          type="text"
-          placeholder="비밀번호를 입력해 주세요"
-        />
-        <p className="text-red text-xs mb-2">*미입력 알림 텍스트 부분</p>
-      </div>
-      <div>
-        <p className="text-base font-semibold mb-2">Pw</p>
-        <input
-          className="w-full border-2 border-gray-300 p-3 h-12
-          rounded-lg focus:border-primary focus:border-2 focus:outline-none mb-2"
-          type="text"
-          placeholder="닉네임을 입력해 주세요"
-        />
-        <p className="text-red text-xs mb-2">*미입력 알림 텍스트 부분</p>
-      </div>
-      <div>
-        <p className="text-base font-semibold mb-2">생년월일</p>
-        <input
-          className="w-full border-2 border-gray-300 p-3 h-12
-          rounded-lg focus:border-primary focus:outline-none 
-          focus:border-2 mb-2 block
-          placeholder-gray-400"
-          type="text"
-          placeholder=" 연도-월-일"
-          onFocus={e => (e.target.type = "date")}
-          onBlur={e => {
-            if (!e.target.value) e.target.type = "text";
-          }}
-        />
-        <p className="text-red text-xs mb-2">*미입력 알림 텍스트 부분</p>
+
+      <InputField
+        errorMsg="미입력 알림 텍스트 부분"
+        labelName="닉네임"
+        placeholder="닉네임을 입력해 주세요"
+      ></InputField>
+      <InputField
+        errorMsg="미입력 알림 텍스트 부분"
+        labelName="이메일"
+        placeholder="이메일을 입력해 주세요"
+      ></InputField>
+      <InputField
+        errorMsg="미입력 알림 텍스트 부분"
+        labelName="Id"
+        placeholder="아이디를 입력해 주세요"
+      ></InputField>
+      <InputField
+        errorMsg="미입력 알림 텍스트 부분"
+        labelName="Pw"
+        placeholder="비밀번호를 입력해 주세요"
+      ></InputField>
+      <div className="mb-9">
+        <InputField
+          errorMsg="미입력 알림 텍스트 부분"
+          labelName="생년월일"
+          type="date"
+          placeholder="연도-월-일"
+        ></InputField>
       </div>
 
       <button className="h-12 bg-primary text-white font-bold text-xl w-full mb-4">
