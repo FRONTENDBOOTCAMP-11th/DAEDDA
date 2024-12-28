a9e3cd75a93205e526a8c737f6e30839a971133f
Donghwan Oh <imodh97@gmail.com>
2024-12-28 14:02:19 +0900

    ✨ 헤더 고정, 컨텐츠 영역 스크롤롤

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,7 +1,7 @@
 export default function MainHeader() {
   return (
-    <header className="w-full h-[60px] flex items-center justify-between mb-5">
-      <img src="/src/assets/logos/header-logo.png" className="w-[190px] ml-1" />
+    <header className="w-full h-[60px] flex items-center justify-between fixed top-0 left-0 bg-white px-6">
+      <img src="/src/assets/logos/header-logo.png" className="w-[190px]" />
       <div className="flex items-center gap-4">
         <img src="/icons/alarm.svg" />
         <img src="/icons/hamburger.svg" className="w-6" />
