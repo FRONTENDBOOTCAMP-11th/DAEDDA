c1e8c3d39cd255a6c50ce93bc36f7c966fd383fe
Donghwan Oh <imodh97@gmail.com>
2024-12-28 14:54:06 +0900

    🐛 fix: 헤더, 푸터 사이즈 조정, 가운데 정렬렬

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,6 +1,6 @@
 export default function MainHeader() {
   return (
-    <header className="w-full h-[60px] flex items-center justify-between fixed top-0 left-0 bg-white px-6">
+    <header className="w-full h-[60px] flex items-center justify-between fixed top-0 max-w-screen-sm left-1/2 -translate-x-1/2 bg-white px-6">
       <img src="/src/assets/logos/header-logo.png" className="w-[190px]" />
       <div className="flex items-center gap-4">
         <img src="/icons/alarm.svg" />
