d5f340907666df6b172e2f2b74c8846b09a7eb4d
JeonSuna <jsa020910@gmail.com>
2024-12-29 14:56:32 +0900

    💄 style: 관심리스트 페이지 1차 완성

--- src/pages/myPage/Likes.jsx
+++ src/pages/myPage/Likes.jsx
@@ -1,31 +1,133 @@
 export default function Likes() {
   return (
-    <div className="shadow-custom-shadow rounded-3xl px-6 pt-4 mb-8">
-      <div className="flex ">
-        <p className="text-sm">기장대게 할인 마트</p>
-        <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
-      </div>
-      <p className="text-lg font-bold">기장대게 할인마트 홀 서빙 대타 구함!</p>
-      <div className="flex gap-[6px] items-center">
-        <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
-        <p className="text-xs">민락동</p>
+    <>
+      <div className="shadow-custom-shadow rounded-3xl px-6 pt-4 mb-8">
+        <div className="flex ">
+          <p className="text-sm">기장대게 할인 마트</p>
+          <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
+        </div>
+        <p className="text-lg font-bold">
+          기장대게 할인마트 홀 서빙 대타 구함!
+        </p>
+        <div className="flex gap-[6px] items-center">
+          <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
+          <p className="text-xs">민락동</p>
+        </div>
+
+        <div className="flex items-center">
+          <div>
+            <div className="flex gap-2 ">
+              <p className="text-secondary font-semibold">시급</p>
+              <p>15,000원</p>
+            </div>
+            <p className="text-beige-500 font-semibold text-xs pb-3">
+              1시간 전
+            </p>
+          </div>
+          <button className="rounded-2xl border border-primary ml-auto">
+            <div className="flex gap-1 px-2 items-center">
+              <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
+              <p className="text-primary text-sm">지원</p>
+            </div>
+          </button>
+        </div>
       </div>
 
-      <div className="flex items-center">
-        <div>
-          <div className="flex gap-2 ">
-            <p className="text-secondary font-semibold">시급</p>
-            <p>15,000원</p>
-          </div>
-          <p className="text-beige-500 font-semibold text-xs pb-3">1시간 전</p>
+      <div className="shadow-custom-shadow rounded-3xl px-6 pt-4 mb-8">
+        <div className="flex ">
+          <p className="text-sm">기장대게 할인 마트</p>
+          <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
         </div>
-        <button className="rounded-2xl border border-primary ml-auto">
-          <div className="flex gap-1 px-2 items-center">
-            <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
-            <p className="text-primary text-sm">지원</p>
+        <p className="text-lg font-bold">
+          기장대게 할인마트 홀 서빙 대타 구함!
+        </p>
+        <div className="flex gap-[6px] items-center">
+          <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
+          <p className="text-xs">민락동</p>
+        </div>
+
+        <div className="flex items-center">
+          <div>
+            <div className="flex gap-2 ">
+              <p className="text-secondary font-semibold">시급</p>
+              <p>15,000원</p>
+            </div>
+            <p className="text-beige-500 font-semibold text-xs pb-3">
+              1시간 전
+            </p>
           </div>
-        </button>
+          <button className="rounded-2xl border border-primary ml-auto">
+            <div className="flex gap-1 px-2 items-center">
+              <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
+              <p className="text-primary text-sm">지원</p>
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
+
+      <div className="shadow-custom-shadow rounded-3xl px-6 pt-4 mb-8">
+        <div className="flex ">
+          <p className="text-sm">기장대게 할인 마트</p>
+          <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
+        </div>
+        <p className="text-lg font-bold">
+          기장대게 할인마트 홀 서빙 대타 구함!
+        </p>
+        <div className="flex gap-[6px] items-center">
+          <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
+          <p className="text-xs">민락동</p>
+        </div>
+
+        <div className="flex items-center">
+          <div>
+            <div className="flex gap-2 ">
+              <p className="text-secondary font-semibold">시급</p>
+              <p>15,000원</p>
+            </div>
+            <p className="text-beige-500 font-semibold text-xs pb-3">
+              1시간 전
+            </p>
+          </div>
+          <button className="rounded-2xl border border-primary ml-auto">
+            <div className="flex gap-1 px-2 items-center">
+              <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
+              <p className="text-primary text-sm">지원</p>
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <div className="shadow-custom-shadow rounded-3xl px-6 pt-4 mb-8">
+        <div className="flex ">
+          <p className="text-sm">기장대게 할인 마트</p>
+          <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
+        </div>
+        <p className="text-lg font-bold">
+          기장대게 할인마트 홀 서빙 대타 구함!
+        </p>
+        <div className="flex gap-[6px] items-center">
+          <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
+          <p className="text-xs">민락동</p>
+        </div>
+
+        <div className="flex items-center">
+          <div>
+            <div className="flex gap-2 ">
+              <p className="text-secondary font-semibold">시급</p>
+              <p>15,000원</p>
+            </div>
+            <p className="text-beige-500 font-semibold text-xs pb-3">
+              1시간 전
+            </p>
+          </div>
+          <button className="rounded-2xl border border-primary ml-auto">
+            <div className="flex gap-1 px-2 items-center">
+              <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
+              <p className="text-primary text-sm">지원</p>
+            </div>
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
