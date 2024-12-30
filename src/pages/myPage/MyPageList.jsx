5c74a4e241a9de8e02023ffa334856b8d28ec5aa
JeonSuna <jsa020910@gmail.com>
2024-12-30 21:58:32 +0900

    list 컴포넌트 수정

--- src/pages/myPage/MyPageList.jsx
+++ src/pages/myPage/MyPageList.jsx
@@ -12,6 +12,7 @@ export default function MyPageList({ icon, label }) {
     <div className="flex mb-4 items-center">
       <img src={`/icons/${icon}.svg`} alt={label} className="mr-3" />
       <p className="text-xl">{label}</p>
+      <img src="/icons/arrow.svg" alt="" className="ml-auto size-4 mb-2" />
     </div>
   );
 }
