0cc85429f06b1b514af3242974130680d58dadf8
Donghwan Oh <imodh97@gmail.com>
2024-12-30 22:13:09 +0900

    💄 style: 상단 패딩 제거, 하단 패딩 추가가

--- src/pages/main/MainList.jsx
+++ src/pages/main/MainList.jsx
@@ -1,7 +1,7 @@
 export default function MainList() {
   return (
-    <div>
-      <div className="flex justify-between items-center text-[24px] font-semibold py-[23px]">
+    <div className="mb-[80px]">
+      <div className="flex justify-between items-center text-[24px] font-semibold pb-[23px]">
         <p>동래구 중앙대로 1473번길 14-2</p>
         <div className="flex gap-4 screen-530:hidden">
           <img src="/icons/search.svg" />
