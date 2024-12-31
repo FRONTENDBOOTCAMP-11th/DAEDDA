d12263498d8d4e553869159592f044275aeaf1bf
yeon1ee <carpediem.y13@gmail.com>
2024-12-31 14:24:08 +0900

    🚧 cont: splash 화면 작업중

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "../routes";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(false);
+  useEffect(() => {
+    //스플래쉬 화면이 실행 되었는지 sessionStorage에 저장한다.
+    const isShown = sessionStorage.getItem("isShown");
+
+    if (!isShown) {
+      setShowSplash(true);
+      sessionStorage.setItem("showSplash", "true");
+
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  });
+
   return (
     <div className="px-6 max-w-screen-sm m-auto h-screen overflow-y-auto relative">
+      {}
       <RouterProvider router={router} future={{ v7_startTransition: true }} />
     </div>
   );
+}
+
+function SplashScreen() {
+  return <div>스플래쉬</div>;
 }
 
 export default App;
