7e6c3ec5081a4082f510fc6d241eed84d0a51199
Donghwan Oh <imodh97@gmail.com>
2024-12-31 10:47:21 +0900

    ✨ feat: axios 훅에 client-id 추가가

--- src/hooks/useAxiosInstance.js
+++ src/hooks/useAxiosInstance.js
@@ -8,6 +8,7 @@ function useAxiosInstance() {
       "Content-Type": "application/json", // request의 데이터 타입
       accept: "application/json", // response의 데이터 타입
       // 판매 유저로 임시 로그인
+      "client-id": "final01",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIsInR5cGUiOiJzZWxsZXIiLCJuYW1lIjoi64Sk7JikIiwiZW1haWwiOiJzMUBtYXJrZXQuY29tIiwiaW1hZ2UiOiIvZmlsZXMvZmluYWwwMS91c2VyLW5lby53ZWJwIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzU2MDg2NjksImV4cCI6MTczNTY5NTA2OSwiaXNzIjoiRkVTUCJ9.M3fLsonLAEv8NBY67Ul5Fn4U4BLJl-8lAiYsVJJUXEc",
     },
