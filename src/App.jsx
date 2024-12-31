0d27d5208e6239839a6b6cdd350992698f9a489b
yeon1ee <carpediem.y13@gmail.com>
2024-12-31 15:03:07 +0900

    ⚰️ cleanup: App.jsx 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "../routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  const [showSplash, setShowSplash] = useState(false);
-  useEffect(() => {
-    //스플래쉬 화면이 실행 되었는지 sessionStorage에 저장한다.
-    const isShown = sessionStorage.getItem("isShown");
-
-    if (!isShown) {
-      setShowSplash(true);
-      sessionStorage.setItem("showSplash", "true");
-
-      const timer = setTimeout(() => {
-        setShowSplash(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }
-  });
-
   return (
-    <QueryClientProvider client={QueryClient}>
-      <div className="px-6 max-w-screen-sm m-auto h-screen overflow-y-auto relative">
-        <RouterProvider router={router} future={{ v7_startTransition: true }} />
-      </div>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <div className="px-6 max-w-screen-sm m-auto h-screen overflow-y-auto relative">
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </div>
   );
-}
-
-function SplashScreen() {
-  return <div>스플래쉬</div>;
 }
 
 export default App;
