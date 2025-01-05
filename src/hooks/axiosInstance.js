77a4382eb2cab81c5255183e773734a8d4dabce2
Donghwan Oh <imodh97@gmail.com>
2025-01-05 21:26:54 +0900

    ♻️ refactor: axiosInstance 파일 추가가

---
+++ src/hooks/axiosInstance.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: "https://11.fesp.shop",
+  timeout: 1000 * 15,
+  headers: {
+    "Content-Type": "application/json", // request의 데이터 타입
+    accept: "application/json", // response의 데이터 타입
+    // 판매 유저로 임시 로그인
+    "client-id": "final01",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIsInR5cGUiOiJzZWxsZXIiLCJuYW1lIjoi64Sk7JikIiwiZW1haWwiOiJzMUBtYXJrZXQuY29tIiwiaW1hZ2UiOiIvZmlsZXMvZmluYWwwMS91c2VyLW5lby53ZWJwIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzU4Njk5NzQsImV4cCI6MTczODQ2MTk3NCwiaXNzIjoiRkVTUCJ9.DG3Ap4YpO6gELIrOWs4xnUDPIf6cT6Ia6uI0nGNsE5M",
+  },
+});
+
+export default axiosInstance;
