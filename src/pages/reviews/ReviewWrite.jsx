b2751d5e2cd91ddcce53a1599d5c099d3b4f1584
Donghwan Oh <imodh97@gmail.com>
2025-01-06 23:41:28 +0900

    ✨ feat: 리뷰를 등록할 상품 데이터 뿌리기, 리뷰 작성 서버 요청청

--- src/pages/reviews/ReviewWrite.jsx
+++ src/pages/reviews/ReviewWrite.jsx
@@ -1,14 +1,21 @@
 import Button from "@components/layout/Button";
 import InputField from "@components/layout/InputField";
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useGetPost } from "@hooks/useGetPost";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useLocation, useParams } from "react-router-dom";
+import { Rating } from "react-simple-star-rating";
+import { formatDate, getTimePassed, getWorkTime } from "@/utills/func.js";
 
 export default function ReviewWrite() {
   const { _id: post_id } = useParams();
   const location = useLocation();
   const writeTo = location.state;
+  const [rating, setRating] = useState(0);
+
+  const { data } = useGetPost(post_id);
 
   const {
     register,
@@ -16,13 +23,18 @@ export default function ReviewWrite() {
     formState: { errors },
   } = useForm();
 
+  const handleRating = rate => {
+    setRating(rate);
+  };
+
   const axios = useAxiosInstance();
 
   const addReview = useMutation({
     mutationFn: async formData => {
       let body = {
-        order_id: 1,
-        product_id: post_id,
+        order_id: 7,
+        product_id: +post_id,
+        rating,
         content: formData.content,
         extra: {
           title: formData.title,
@@ -40,46 +52,68 @@ export default function ReviewWrite() {
     },
   });
 
+  console.log(data);
+
   return (
-    <div className="mb-[40px]">
-      <div className="flex justify-between mb-4 flex-wrap">
-        <h2 className="text-[20px] font-semibold">
-          {writeTo === "person"
-            ? `내가 맡긴 장소 : 소문난 손칼국수`
-            : `내가 일한 장소 : 소문난 손칼국수`}
-        </h2>
-        <p className="text-gray-500 font-semibold">12/7 (화)ㆍ10:00 ~ 16:00</p>
-      </div>
-      <div className="flex gap-1 mb-5">
+    <>
+      {data && (
+        <div className="mb-[40px]">
+          <div className="flex justify-between mb-4 flex-wrap">
+            <h2 className="text-[20px] font-semibold">
+              {writeTo === "person"
+                ? `내가 맡긴 장소 : ${data.extra.condition.company}`
+                : `내가 일한 장소 : ${data.extra.condition.company}`}
+            </h2>
+            <p className="text-gray-500 font-semibold">
+              {formatDate(data.extra.condition.date)}ㆍ
+              {data.extra.condition.workTime[0]} ~{" "}
+              {data.extra.condition.workTime[1]}
+            </p>
+          </div>
+          <Rating
+            onClick={handleRating}
+            SVGstyle={{ display: "inline" }}
+            transition
+            fillColor="#FEE500"
+            className="mb-5"
+          />
+
+          {/* <div className="flex gap-1 mb-5">
         <img src="/icons/reviews/star.svg" />
         <img src="/icons/reviews/star.svg" />
         <img src="/icons/reviews/star.svg" />
         <img src="/icons/reviews/star.svg" />
         <img src="/icons/reviews/blankStar.svg" />
-      </div>
-      <form onSubmit={handleSubmit(addReview.mutate)}>
-        <InputField
-          labelName="제목"
-          placeholder="제목을 입력해 주세요."
-          register={register("title", { required: "제목 입력은 필수입니다." })}
-          errorMsg={errors.title?.message}
-        />
-        <InputField
-          isTextArea={true}
-          labelName="내용"
-          placeholder="내용을 입력해 주세요."
-          register={register("content", {
-            required: "내용 입력은 필수입니다.",
-          })}
-          errorMsg={errors.content?.message}
-        />
-        <p className="text-[18px] font-semibold mb-4">
-          {writeTo === "person" ? `보낼 금액: 64,000원` : `받을 금액: 64,000원`}
-        </p>
-        <Button type="submit" height="lg">
-          리뷰 쓰기
-        </Button>
-      </form>
-    </div>
+      </div> */}
+          <form onSubmit={handleSubmit(addReview.mutate)}>
+            <InputField
+              labelName="제목"
+              placeholder="제목을 입력해 주세요."
+              register={register("title", {
+                required: "제목 입력은 필수입니다.",
+              })}
+              errorMsg={errors.title?.message}
+            />
+            <InputField
+              isTextArea={true}
+              labelName="내용"
+              placeholder="내용을 입력해 주세요."
+              register={register("content", {
+                required: "내용 입력은 필수입니다.",
+              })}
+              errorMsg={errors.content?.message}
+            />
+            <p className="text-[18px] font-semibold mb-4">
+              {writeTo === "person"
+                ? `보낼 금액: ${data.price.toLocaleString()}원`
+                : `받을 금액: ${data.price.toLocaleString()}원`}
+            </p>
+            <Button type="submit" height="lg">
+              리뷰 쓰기
+            </Button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
