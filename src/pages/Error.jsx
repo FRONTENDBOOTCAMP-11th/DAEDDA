3bd45dbe6960593b863747dcadf31c28c2f9b352
JeonSuna <jsa020910@gmail.com>
2025-01-06 17:33:26 +0900

    💄 style: br추가

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -11,14 +11,15 @@ export default function Error() {
     navigate(-1);
   };
   return (
-    <div className=" -mt-[80px] -mx-6 max-w-screen-sm m-auto h-screen overflow-hidden   flex items-center flex-col bg-[#F8F1FF]">
+    <div className=" -mt-[80px] -mx-6 max-w-screen-sm m-auto h-screen overflow-hidden   flex items-center  flex-col bg-[#F8F1FF] px-5">
       <img src="/images/404_daeddamon.png" alt="" className="mt-20 mb-8" />
-      <p className="font-bold text-3xl mb-12">
-        죄송합니다. 아직 준비중인 기능입니다.
+      <p className="font-bold text-3xl  text-center mb-8">
+        죄송합니다. <br />
+        아직 준비중인 기능입니다.
       </p>
       <p className="text-xl max-w-[480px] text-center mb-8">
-        더 나은 경험을 제공하기 위해 노력하고 있습니다. <br></br>지금은 이용할
-        수 없지만, 다른 멋진 페이지를 둘러보세요! 😊
+        더 나은 경험을 제공하기 위해 노력하고 있습니다. <br />
+        지금은 이용할 수 없지만, 다른 멋진 페이지를 둘러보세요! 😊
       </p>
       <div className="flex gap-11">
         <Button
