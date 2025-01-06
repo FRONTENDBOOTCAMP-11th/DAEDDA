1e8c804f5d5dbf95bbad9c530069c436f3f81733
yeon1ee <carpediem.y13@gmail.com>
2025-01-06 17:35:37 +0900

    🚧 cont: 회원가입 진행중

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -16,6 +16,94 @@ export default function SignUp() {
 
   return (
     <div className="flex flex-col items-center justify-center mb-[40px]">
+<<<<<<< HEAD
+      <form className="w-full" onSubmit={handleSubmit()}>
+        <img
+          src="/src/assets/images/smiling_daeddamon.png"
+          className="w-[150px] h-[150px] mb-3"
+        />
+        <div>
+          <Button color="purple" height="sm" className="mb-4">
+            <span className="p-2">이미지 선택</span>
+          </Button>
+        </div>
+        <div className="w-full">
+          <InputField
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            maxLength="30"
+            errorMsg={errors.email?.message}
+            register={register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "이메일 형식으로 작성해주세요.",
+              },
+            })}
+          ></InputField>
+        </div>
+        <div className="w-full">
+          <InputField
+            type="text"
+            placeholder="닉네임을 입력해주세요."
+            maxLength="10"
+            errorMsg={errors.name?.message}
+            register={register("name", {
+              required: "닉네임을 입력해주세요",
+              minLength: {
+                value: 2,
+                message: "닉네임은 최소 2자리 이상 입력해야 합니다.",
+              },
+            })}
+          ></InputField>
+        </div>
+        <div className="relative w-full">
+          <InputField
+            type={showPwd ? "text" : "password"}
+            placeholder="비밀번호를 입력해주세요."
+            maxLength="20"
+            errorMsg={errors.password?.message}
+            register={register("password", {
+              required: "비밀번호를 입력해주세요.",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 최소 8자리 이상 입력해야 합나디.",
+              },
+            })}
+          ></InputField>
+          <img
+            src={
+              showPwd ? "/public/icons/eye.svg" : "/public/icons/eyeHalf.svg"
+            }
+            className="absolute right-3 top-3"
+            onClick={() => setShowPwd(pre => !pre)}
+          />
+        </div>
+        <div className="relative w-full">
+          <InputField
+            type={showPwdCheck ? "text" : "password"}
+            placeholder="비밀번호를 확인해주세요."
+            maxLength="20"
+            errorMsg={errors.pwdCheck?.message}
+            register={register("pwdCheck", {
+              required: "비밀번호를 확인해주세요.",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 최소 8자리 이상 입력해야 합나디.",
+              },
+            })}
+          ></InputField>
+          <img
+            src={
+              showPwdCheck
+                ? "/public/icons/eye.svg"
+                : "/public/icons/eyeHalf.svg"
+            }
+            className="absolute right-3 top-3"
+            onClick={() => setShowPwdCheck(pre => !pre)}
+          />
+        </div>
+=======
       <img
         src="/images/smiling_daeddamon.png"
         className="w-[150px] h-[150px] mb-3"
@@ -25,42 +113,7 @@ export default function SignUp() {
           <span className="p-2">이미지 선택</span>
         </Button>
       </div>
-
-      <div className="w-full">
-        <InputField
-          type="email"
-          placeholder="이메일을 입력해주세요."
-          errorMsg="이메일을 입력해주세요."
-          maxLength="30"
-        ></InputField>
-      </div>
-
-      <div className="w-full">
-        <InputField
-          type="text"
-          placeholder="닉네임을 입력해주세요."
-          errorMsg="닉네임을 입력해주세요."
-          maxLength="10"
-        ></InputField>
-      </div>
-      <div className="relative w-full">
-        <InputField
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          errorMsg="비밀번호를 입력해주세요."
-          maxLength="20"
-        ></InputField>
-        <img src="/icons/eyeHalf.svg" className="absolute right-3 top-3" />
-      </div>
-      <div className="relative w-full">
-        <InputField
-          type="password"
-          placeholder="비밀번호를 확인해주세요."
-          errorMsg="비밀번호를 입력해주세요."
-          maxLength="20"
-        ></InputField>
-        <img src="/icons/eyeHalf.svg" className="absolute right-3 top-3" />
-      </div>
+>>>>>>> 8fb5ac5fc978051f43eb16e7294ca2cb8cf32eb6
 
         <div className="w-full">
           <InputField
