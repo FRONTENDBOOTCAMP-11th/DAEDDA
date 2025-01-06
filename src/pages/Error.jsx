fccce4d8b3f316692bd11452b4d0ab28cdee6850
JeonSuna <jsa020910@gmail.com>
2025-01-06 16:21:29 +0900

    ⚰️ cleanup:

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -14,7 +14,7 @@ export default function Error() {
     <div className=" -mt-[80px] -mx-6 max-w-screen-sm m-auto h-screen overflow-hidden   flex items-center flex-col bg-[#F8F1FF]">
       <img src="/images/404_daeddamon.png" alt="" className="mt-20 mb-8" />
       <p className="font-bold text-3xl mb-12">
-        죄송합니다. 아직 준비중인 기능입니다
+        죄송합니다. 아직 준비중인 기능입니다.
       </p>
       <p className="text-xl max-w-[480px] text-center mb-8">
         더 나은 경험을 제공하기 위해 노력하고 있습니다. <br></br>지금은 이용할
