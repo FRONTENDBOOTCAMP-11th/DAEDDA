616b171f0d88c37af17aad00f6202816b9930ff1
Donghwan Oh <imodh97@gmail.com>
2025-01-07 17:04:18 +0900

    ♻️ refactor: axios authorization 추가가

--- src/hooks/useAxiosInstance.js
+++ src/hooks/useAxiosInstance.js
@@ -9,8 +9,14 @@ function useAxiosInstance() {
       accept: "application/json", // response의 데이터 타입
       // 판매 유저로 임시 로그인
       "client-id": "final01",
+
+      // 일반 유저 (_id = 4)
+      // Authorization:
+      //   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjQsInR5cGUiOiJ1c2VyIiwibmFtZSI6IuygnOydtOyngCIsImVtYWlsIjoidTFAbWFya2V0LmNvbSIsImltYWdlIjoiL2ZpbGVzL2ZpbmFsMDEvdXNlci1qYXlnLndlYnAiLCJsb2dpblR5cGUiOiJlbWFpbCIsImlhdCI6MTczNjIzMzU4MSwiZXhwIjoxNzM2MzE5OTgxLCJpc3MiOiJGRVNQIn0.-QhYhmM7NzcJt21pnN6lrSgj35OlCwxxcs1WYI_2rMY",
+
+      // 판매 유저 (_id = 2)
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIsInR5cGUiOiJzZWxsZXIiLCJuYW1lIjoi64Sk7JikIiwiZW1haWwiOiJzMUBtYXJrZXQuY29tIiwiaW1hZ2UiOiIvZmlsZXMvZmluYWwwMS91c2VyLW5lby53ZWJwIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzU4Njk5NzQsImV4cCI6MTczODQ2MTk3NCwiaXNzIjoiRkVTUCJ9.DG3Ap4YpO6gELIrOWs4xnUDPIf6cT6Ia6uI0nGNsE5M",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIsInR5cGUiOiJzZWxsZXIiLCJuYW1lIjoi7IiY7KCV7ZWg6rKMIOOFi2RkIiwiZW1haWwiOiJzMUBtYXJrZXQuY29tIiwiaW1hZ2UiOiIvZmlsZXMvZmluYWwwMS9FbTlIRGkzc0Mud2VicCIsImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzM2MjMzNjU2LCJleHAiOjE3MzYzMjAwNTYsImlzcyI6IkZFU1AifQ.LA-mzR0QzbcNl3L45ECN_vM-DBKqynIS5wPW14z74jQ",
     },
   });
 
