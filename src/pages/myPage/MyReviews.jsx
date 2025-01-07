b8bba6ee8da643fad48ac4367cef31d4370e3101
JeonSuna <jsa020910@gmail.com>
2025-01-07 17:13:13 +0900

    ✨ feat: 리뷰페이지 zustand적용

--- src/pages/myPage/MyReviews.jsx
+++ src/pages/myPage/MyReviews.jsx
@@ -1,17 +1,19 @@
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import MyReviewList from "@pages/myPage/MyReviewList";
 import { useQuery } from "@tanstack/react-query";
+import useUserStore from "@zustand/userStore";
 import { useParams } from "react-router-dom";
+import { useStore } from "zustand";
 
 export default function MyReviews() {
   const axios = useAxiosInstance();
 
   // const { _id } = useParams();
-
+  const { user } = useUserStore();
   const { data } = useQuery({
     queryKey: ["reviews"],
     //일단 뿌리기 작업 test를 위해 seller 아이디를 2로 하드코딩, 이후에 로그인 작업이 완료되면 세션스토리지에서 id꺼내 올 예정
-    queryFn: () => axios.get(`/replies/seller/2`),
+    queryFn: () => axios.get(`/replies/seller/${user._id}`),
     select: res => res.data,
     staleTime: 1000 * 10,
   });
@@ -21,8 +23,6 @@ export default function MyReviews() {
   // 댓글 수 계산
   let totalReplies = 0;
   data.item.forEach(item => (totalReplies += item.replies.length));
-  // console.log(totalReplies);
-  // console.log(data.item);
 
   const list = data.item.map(item => (
     <MyReviewList key={item._id} item={item} />
