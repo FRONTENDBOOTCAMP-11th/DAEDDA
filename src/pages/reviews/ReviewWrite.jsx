5060b97c01e421a87b270a15ecdccd7cdf9910fd
Donghwan Oh <imodh97@gmail.com>
2025-01-08 15:20:59 +0900

    ♻️ refactor: 주문 API 사용, 컴포넌트 리팩토링링

--- src/pages/reviews/ReviewWrite.jsx
+++ src/pages/reviews/ReviewWrite.jsx
@@ -1,24 +1,20 @@
 import Button from "@components/layout/Button";
 import InputField from "@components/layout/InputField";
 import useAxiosInstance from "@hooks/useAxiosInstance";
-import { useGetPost } from "@hooks/useGetPost";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Rating } from "react-simple-star-rating";
 import { formatDate } from "@/utills/func.js";
-import { useGetOrder } from "@hooks/useGetOrder";
 
 export default function ReviewWrite() {
-  const { _id: product_id } = useParams();
   const location = useLocation();
-  const from = location.state;
-  const [rating, setRating] = useState(0);
   const navigate = useNavigate();
+  const { position, order } = location.state;
 
-  const { data } = useGetPost(product_id);
-  const { data: order } = useGetOrder(product_id);
+  const [rating, setRating] = useState(0);
+
   const {
     register,
     handleSubmit,
@@ -31,35 +27,53 @@ export default function ReviewWrite() {
 
   const axios = useAxiosInstance();
 
-  if (order && data)
-    console.log("order: " + order[0]._id, "product id" + data._id);
+  const editOrderState = useMutation({
+    mutationFn: async state => {
+      return axios.patch(`/orders/${order._id}`, { state });
+    },
+
+    onSuccess: response => {
+      console.log("입금 완료 상태 변경");
+      console.log(response);
+    },
+    onError: error => {
+      console.error("등록 실패:", error);
+    },
+  });
 
   const addReview = useMutation({
     mutationFn: async formData => {
-      let body = {
-        order_id: order[0]._id,
-        product_id: +product_id,
+      const body = {
+        order_id: order._id,
+        product_id: order.products[0]._id,
         rating,
         content: formData.content,
         extra: {
           title: formData.title,
-          from,
+          position,
         },
       };
 
       return axios.post("/replies", body);
     },
     onSuccess: response => {
-      if (from === "hired")
+      // employer가 리뷰 작성
+      if (position === "employer") {
         alert(
-          `리뷰가 등록되었습니다. 일당 ${data.price.toLocaleString()}원이 정상적으로 전달되었습니다.`,
-          // worker의 state를 수금 완료로 변경
+          `리뷰가 등록되었습니다. 일당 ${order.products[0].price.toLocaleString()}원이 정상적으로 전달되었습니다.`,
         );
+        // worker의 state를 수금 완료로 변경
+        editOrderState.mutate("EM040");
+      }
+      // worker가 리뷰 작성
       else {
         alert(
-          `리뷰가 등록되었습니다. 일당 ${data.price.toLocaleString()}원 입금을 다시한 번 확인해 주세요.`,
+          `리뷰가 등록되었습니다. 일당 ${order.products[0].price.toLocaleString()}원 입금을 확인해 주세요.`,
         );
+        editOrderState.mutate("WO040");
       }
+
+      console.log(response);
       navigate(-1);
     },
     onError: error => {
@@ -69,18 +83,18 @@ export default function ReviewWrite() {
 
   return (
     <>
-      {data && (
+      {order && (
         <div className="mb-[40px]">
           <div className="flex justify-between mb-4 flex-wrap">
             <h2 className="text-[20px] font-semibold">
-              {from === "hired"
-                ? `내가 맡긴 장소 : ${data.extra.condition.company}`
-                : `내가 일한 장소 : ${data.extra.condition.company}`}
+              {position === "employer"
+                ? `내가 맡긴 장소 : ${order.products[0].extra.condition.company}`
+                : `내가 일한 장소 : ${order.products[0].extra.condition.company}`}
             </h2>
             <p className="text-gray-500 font-semibold">
-              {formatDate(data.extra.condition.date)}ㆍ
-              {data.extra.condition.workTime[0]} ~{" "}
-              {data.extra.condition.workTime[1]}
+              {formatDate(order.products[0].extra.condition.date)}ㆍ
+              {order.products[0].extra.condition.workTime[0]} ~{" "}
+              {order.products[0].extra.condition.workTime[1]}
             </p>
           </div>
           <Rating
@@ -109,9 +123,9 @@ export default function ReviewWrite() {
               errorMsg={errors.content?.message}
             />
             <p className="text-[18px] font-semibold mb-4">
-              {from === "hired"
-                ? `보낼 금액: ${data.price.toLocaleString()}원`
-                : `받을 금액: ${data.price.toLocaleString()}원`}
+              {position === "employer"
+                ? `보낼 금액: ${order.products[0].price.toLocaleString()}원`
+                : `받을 금액: ${order.products[0].price.toLocaleString()}원`}
             </p>
             <Button type="submit" height="lg">
               리뷰 쓰기
