a0240079212ed51e4a8767df8877cc86ffd40da5
JeonSuna <jsa020910@gmail.com>
2025-01-08 09:30:54 +0900

    ✨ feat: zustand수정

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,31 +1,28 @@
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import MyPageList from "@pages/myPage/MyPageList";
 import { useQuery } from "@tanstack/react-query";
+import useUserStore from "@zustand/userStore";
 import { Link } from "react-router-dom";
 
 export default function Profile() {
   const axios = useAxiosInstance();
-  const { data } = useQuery({
-    queryKey: ["user", "userId"],
-    queryFn: () => axios.get(`/users/2`),
-    select: res => res.data,
-    staleTime: 1000 * 10,
-  });
-  console.log(data);
+  const { user } = useUserStore();
+  console.log(user);
+  console.log(user.image);
 
   return (
     <div className="mb-[40px]">
       <div className="flex flex-col items-center border-b mb-8">
         <img
           src={
-            data?.item?.image
-              ? `https://11.fesp.shop/${data.item.image}`
+            user.image
+              ? `https://11.fesp.shop/${user.image}`
               : "/images/smiling_daeddamon.png"
           }
           alt="프로필 이미지"
           className="size-48 mb-4 mt-6"
         />
-        <p className="font-bold text-4xl mb-6">{data.item.name}</p>
+        <p className="font-bold text-4xl mb-6">{user.name}</p>
       </div>
 
       <div className="myPage-container pt-5 flex-col flex pb-6">
