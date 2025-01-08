9366456a87f4e90171d22cc921b57500c6582465
JeonSuna <jsa020910@gmail.com>
2025-01-08 15:00:10 +0900

    ✨ feat: 북마크 기능 완료, css수정

--- src/pages/myPage/LikeList.jsx
+++ src/pages/myPage/LikeList.jsx
@@ -1,5 +1,7 @@
 import { getWorkTime } from "@/utills/func";
 import { getTimePassed } from "@/utills/func";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -7,51 +9,74 @@ LikeList.propTypes = {
   item: PropTypes.object.isRequired,
 };
 export default function LikeList({ item }) {
+  const axios = useAxiosInstance();
+  const queryClient = useQueryClient();
+  const removeLike = useMutation({
+    mutationFn: _id => axios.delete(`/bookmarks/${_id}`),
+    onSuccess: () => {
+      alert("좋아요가 삭제되었습니다");
+      queryClient.invalidateQueries({ queryKey: ["product"] });
+    },
+    onError: err => {
+      console.error(err);
+    },
+  });
+
   console.log(item);
-  console.log(item.product.extra?.condition?.name);
   return (
     <>
-      <div className="likes-container">
-        <div className="flex items-center ">
-          <p className="text-sm mt-1">
-            {item.product.extra?.condition?.company || "못된고양이"}
-          </p>
-          <img src="/icons/likes.svg" alt="찜하기 버튼" className="ml-auto" />
-        </div>
-        <p className="text-lg font-bold">{item.product.name}</p>
-        <div className="flex gap-[6px] items-center">
-          <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
-          <p className="text-xs">민락동</p>
-        </div>
-        <div className="flex items-center">
-          <div className="font-semibold">
-            <div className="flex gap-2">
-              <p className="text-secondary">시급</p>
-              <p>
-                {Math.round(
-                  item.product.price /
-                    getWorkTime(
-                      item.product.extra?.condition?.workTime[0] || "09:00",
-                      item.product.extra?.condition?.workTime[1] || "17:00",
-                    ),
-                ).toLocaleString()}
-                원
+      <Link to={`/main/${item.product._id}`}>
+        <div className="likes-container">
+          <div className="flex items-center ">
+            <p className="text-sm">
+              {item.product.extra?.condition?.company || "못된고양이"}
+            </p>
+            <img
+              src="/icons/likes.svg"
+              alt="찜하기 버튼"
+              className="ml-auto"
+              onClick={() => removeLike.mutate(item._id)}
+            />
+          </div>
+          <p className="text-lg font-bold">{item.product.name}</p>
+          <div className="flex gap-[6px] items-center">
+            <img src="/icons/mapPin.svg" alt="위치 표시" className="size-3" />
+            <p className="text-xs">민락동</p>
+          </div>
+          <div className="flex items-center">
+            <div className="font-semibold">
+              <div className="flex gap-2">
+                <p className="text-secondary">시급</p>
+                <p>
+                  {Math.round(
+                    item.product.price /
+                      getWorkTime(
+                        item.product.extra?.condition?.workTime[0] || "09:00",
+                        item.product.extra?.condition?.workTime[1] || "17:00",
+                      ),
+                  ).toLocaleString()}
+                  원
+                </p>
+              </div>
+              <p className="text-beige-500 text-xs pb-3">
+                마감 : {item.product.extra?.condition?.date || "하루 전"}
               </p>
             </div>
-            <p className="text-beige-500 text-xs pb-3">
-              {getTimePassed(item.createdAt)}
-            </p>
+            <button className="rounded-2xl border border-primary ml-auto">
+              <Link to={`/pr/write`}>
+                <div className="flex gap-1 px-2 items-center">
+                  <img
+                    src="/icons/apply.svg"
+                    alt="지원 버튼"
+                    className="mb-1"
+                  />
+                  <p className="text-primary text-sm">지원</p>
+                </div>
+              </Link>
+            </button>
           </div>
-          <button className="rounded-2xl border border-primary ml-auto">
-            <Link to={`/main/${item.product._id}`}>
-              <div className="flex gap-1 px-2 items-center">
-                <img src="/icons/apply.svg" alt="지원 버튼" className="mb-1" />
-                <p className="text-primary text-sm">지원</p>
-              </div>
-            </Link>
-          </button>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
