05e2109a3b05d08654eb4d3c6dc248e26149889c
JeonSuna <jsa020910@gmail.com>
2025-01-09 21:35:55 +0900

    💄 style: 좋아요 한 글이 없을 때 ui 표시

--- src/pages/myPage/Likes.jsx
+++ src/pages/myPage/Likes.jsx
@@ -10,10 +10,18 @@ export default function Likes() {
     select: res => res.data.item,
     staleTime: 1000 * 10,
   });
-  if (!data) {
+  if (data.length === 0) {
+    return (
+      <div className="-mt-[80px] max-w-screen-sm m-auto h-screen overflow-y-auto flex items-center justify-center text-center text-xl text-gray-300">
+        관심 표시한 글이 없어요.
+        <br /> 공고 리스트에 올라온 글을 탐색하고 관심 표시를 해보세요!
+      </div>
+    );
+  }
+  if (isLoading) {
     return <div>로딩중</div>;
   }
-  // console.log(data);
+  console.log(data);
   const list = data.map(item => <LikeList key={item.id} item={item} />);
   return <div className="mb-[80px]">{list}</div>;
 }
