26400ca71a8250b4b44069e04a6a0bf7ec61103c
Donghwan Oh <imodh97@gmail.com>
2025-01-14 22:04:16 +0900

    ✨ feat: 알바 내역 검색 구현현

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -19,6 +19,7 @@ export default function InputField({
   isLast = false,
   isTextArea = false,
   register,
+  className,
   ...props
 }) {
   // type에 따라 전달할 props 객체를 다르게 저장하는 변수
@@ -49,7 +50,7 @@ export default function InputField({
       ) : (
         <input
           {...dateTypeProps}
-          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"}`}
+          className={`w-full h-[48px] p-3 border-2 border-#999 rounded-lg focus:outline-none focus:border-primary ${errorMsg ? "mb-2" : isLast ? "" : "mb-[28px]"} ${className}`}
           {...register}
           {...props}
         />
