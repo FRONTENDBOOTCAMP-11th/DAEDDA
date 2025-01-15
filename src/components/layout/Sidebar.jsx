291ca4741d43d728a440884fb333d479a8c17b33
yeon1ee <carpediem.y13@gmail.com>
2025-01-15 11:46:39 +0900

    🚧 cont: 사이드바 작업중

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,7 +1,17 @@
 export default function Sidebar({ isOpen, toggleSidebar }) {
   return (
-    <div className="fixed top-0 h-screen bg-gray-100 z-20">
-      <h1>sidebar</h1>
+    <div className="absolute top-0 right-0 w-1/2 h-screen bg-gray-100 z-10">
+      <div className="mt-6 ml-6">
+        <img
+          src="/icons/x.svg"
+          className="cursor-pointer"
+          onClick={toggleSidebar}
+        />
+        <div className="flex mt-10 justify-between ">
+          <h1>로그인 하세요</h1>
+          <img src="/icons/arrow.svg" />
+        </div>
+      </div>
     </div>
   );
 }
