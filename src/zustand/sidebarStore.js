bf26b3f314c53f848fda8db3353a95fbcb128933
yeon1ee <carpediem.y13@gmail.com>
2025-01-17 11:30:25 +0900

    ♻️ refactor: 사이드바 전역 상태 관리 변경

--- src/zustand/sidebarStore.js
+++ src/zustand/sidebarStore.js
@@ -4,8 +4,6 @@ import { persist, createJSONStorage } from "zustand/middleware";
 const SidebarStore = set => ({
   isSidebarOpen: false,
   setSidebarOpen: isOpen => set(() => ({ isSidebarOpen: isOpen })),
-  // openSidebar: () => set({ isSidebarOpen: true }), // 사이드바 열리면 true
-  // closeSidebar: () => set({ isSidebarOpen: false }), // 닫히면 false
 });
 
 // 스토리지를 사용할 경우
