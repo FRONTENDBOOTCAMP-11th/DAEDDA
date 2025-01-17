cd64c71bf70c783303f1d847451965e020aff97b
Donghwan Oh <imodh97@gmail.com>
2025-01-17 14:35:12 +0900

    ♻️ refactor: sidebar 전역 상태 스토리지 저장 헤제제

--- src/zustand/sidebarStore.js
+++ src/zustand/sidebarStore.js
@@ -1,17 +1,8 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
 
-const SidebarStore = set => ({
+const useSidebarStore = create(set => ({
   isSidebarOpen: false,
   setSidebarOpen: isOpen => set(() => ({ isSidebarOpen: isOpen })),
-});
-
-// 스토리지를 사용할 경우
-const useSidebarStore = create(
-  persist(SidebarStore, {
-    name: "sidebar",
-    storage: createJSONStorage(() => sessionStorage),
-  }),
-);
+}));
 
 export default useSidebarStore;
