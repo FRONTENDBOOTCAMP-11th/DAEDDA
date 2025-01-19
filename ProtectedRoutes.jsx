835d9f2232e6833a8c078a7f964dca1d215b1605
yeon1ee <carpediem.y13@gmail.com>
2025-01-19 15:20:48 +0900

    ♻️ refactor: replace={true} 추가

--- ProtectedRoutes.jsx
+++ ProtectedRoutes.jsx
@@ -6,7 +6,7 @@ const ProtectedRoute = () => {
 
   if (!user) {
     alert("로그인 필요 - protected route");
-    return <Navigate to="/user/signIn" replace />;
+    return <Navigate to="/user/signIn" replace={true} />;
   }
   return <Outlet />;
 };
