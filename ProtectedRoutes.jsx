69fe12b6c73553d49325f4b7dd4dd11a7b7e3ea1
yeon1ee <carpediem.y13@gmail.com>
2025-01-20 10:55:37 +0900

    ⚰️ cleanup: alert 메세지 수정

--- ProtectedRoutes.jsx
+++ ProtectedRoutes.jsx
@@ -5,7 +5,7 @@ const ProtectedRoute = () => {
   const user = useUserStore(state => state.user);
 
   if (!user) {
-    alert("로그인이 필요한 페이지입니다.");
+    alert("로그인이 필요합니다.");
     return <Navigate to="/user/signIn" replace={true} />;
   }
   return <Outlet />;
