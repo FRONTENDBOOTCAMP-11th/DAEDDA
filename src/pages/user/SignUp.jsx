510e89240edc68584a12590116910f253a9f7899
yeon1ee <carpediem.y13@gmail.com>
2025-02-25 18:11:48 +0900

    ♻️ refactor: (회원가입 페이지) 필수 필드 미기입시 계속 버튼 비활성화

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -49,6 +49,17 @@ export default function SignUp() {
     },
   });
 
+  const watchField = watch([
+    "email",
+    "name",
+    "password",
+    "pwdCheck",
+    "birthday",
+    "phone",
+  ]);
+
+  const isFilled = watchField.every(value => value && value.trim() !== "");
+
   // 이미지 프리뷰 변경
   const handleImageChange = e => {
     const file = e.target.files[0];
@@ -291,7 +302,12 @@ export default function SignUp() {
           <Button color="white" height="lg" onClick={handleCancel}>
             취소
           </Button>
-          <Button color="purple" height="lg" type="submit">
+          <Button
+            color="bg-gray-400"
+            height="lg"
+            type="submit"
+            className={`w-full font-bold rounded text-white ${!isFilled ? "bg-gray-400 cursor-not-allowed" : "bg-primary cursor-pointer"}`}
+          >
             계속
           </Button>
         </div>
